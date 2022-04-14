77074f7c2881ce3e2017af68a7b7b0bbd7cf254f
Luis Fernando Tapia <88354259+Luisf718@users.noreply.github.com>
2022-04-14 12:29:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query('SELECT * FROM PUBLIC.accommodations;')
+rows = run_query("SELECT * FROM PUBLIC.accommodations;")
 
 # Print results.
 for row in rows:
